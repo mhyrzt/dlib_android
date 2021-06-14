3ed2e49b5a0c1b161cf5c1ed670673b552515e5a
Mahyar Riazati <mr.riazati1999@gmail.com>
2021-06-14 18:11:37 +0430

    fixes

--- dlib_utils.hpp
+++ dlib_utils.hpp
@@ -11,7 +11,7 @@ namespace myu {
     void convertBytes(dlib::array2d<dlib::rgb_pixel>&, unsigned char*, int, int);
     std::vector<dlib::full_object_detection> predictLandmakars(
         unsigned char*, int, int, 
-        dlib::frontal_face_detector, 
+        dlib::frontal_face_detector&, 
         dlib::shape_predictor&);
 }
 
